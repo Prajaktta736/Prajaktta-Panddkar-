e5d00e35f2597ca549b8a9814be131976bcee145
Prajaktta736 <67309264+Prajaktta736@users.noreply.github.com>
2020-06-23 15:46:35 +0530

    Java program for the given coding challenge

---
+++ Testcase.java
@@ -0,0 +1,65 @@
+
+
+package Testcase;
+import java.util.*;
+
+	
+public abstract class Testcase {
+
+public static void main(String[] args) {
+	
+
+System.out.println("ENTER THE TESTCASE");	
+
+Scanner T = new Scanner(System.in);
+
+
+
+final int testcases ;int[] T1;int[] T2;
+{
+//T
+	StringBuffer Tc = new StringBuffer(); //created testcase T
+
+	
+	int Ta=1;
+		if(Ta<=100)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+
+	//Pattern
+	StringBuffer pattern = new StringBuffer();
+	int i=1;
+	{
+		if(i<=1000)
+		{
+		System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+	//Text string
+	
+		 StringBuffer textstring = new StringBuffer();
+		int t=1;
+		{
+			if(t<=10000)
+			{
+				System.out.println("yes");
+			}
+			else
+			{
+				System.out.println("no");
+			}
+		}
+	}
+}
+}}
+	
+
+	
